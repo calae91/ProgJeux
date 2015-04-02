ff7f7b88a944280ee9d0987b455e15f52e018431
yobero <yoannb94@gmail.com>
2015-04-02 16:04:18 +0200

    Update league_of_cube_V1,1.c

--- league_of_cube_V1,1.c
+++ league_of_cube_V1,1.c
@@ -1,1 +1,14 @@
+#include "graphics.h"
+
+//Constante
+#define L 500															//hauteur
+#define l 500															//longueur
+
+int main ()
+{
+	init_graphics(l,L);
+	
+wait_escape();
+exit(0);
+}
 
