503460771720596d39047706494dc990519cb722
yobero <yoannb94@gmail.com>
2015-04-09 16:19:01 +0200

    Ajout de la fonction affiche_boule +
    Ajout d'un oublie ds l'initialisation
    +la fonction init_boule ne se termine pas

--- candy_boule.c
+++ candy_boule.c
@@ -4,16 +4,8 @@
 #define LON 320 //multiple de 8 (8 cases)
 #define LAR 2*LON // (16 cases)
 #define E LON/8
-
-struct boule {
-	POINT centre; // position de la boule
-	COULEUR c; // couleur de la boulea#include "graphics.h"
-
-//128 cases au total
-#define LON 320 //multiple de 8 (8 cases)
-#define LAR 2*LON // (16 cases)
-#define E LON/8
 #define D 4 //centre de la case
+#define R 3 //rayon du cercle
 
 struct boule {
 	POINT centre; // position de la boule
@@ -65,6 +57,7 @@ void init_boule(BOULE b[128])
 	int a ;
 	
 	b[0].centre.x = D; b[0].centre.y = D;
+	b[0].c=bleu;
 	
 	while (n<128)
 	{
@@ -93,69 +86,26 @@ void init_boule(BOULE b[128])
 	}
 }
 
+void affiche_boule(BOULE b)
+{
+	draw_fill_circle(b.centre,R,b.c);
+}
+
 int main ()
 {
 	init_graphics(LON,LAR);
 	
 	BOULE b[128];
+	int n=0;
 	
 	affichage_plateau();
-	
 	init_boule(b);
 	
-// Fin du code
-wait_escape();
-exit(0);
-}
-
-	int v; // valeur de la boule (1,2,3,..) en fonction du nombre de couleur
-};
-typedef struct boule BOULE ;
-
-void affichage_plateau_H(POINT p)
-{
-	POINT p1;
-	
-	p1.x = LON; p1.y = p.y;
-	
-	while (p.y<LAR)
+	while (n<128)
 	{
-		draw_line(p,p1,noir);
-		p.y = p.y +E;
-		p1.y=p.y;
+		affiche_boule(b[n]);
+		n++;
 	}
-}
-
-void affichage_plateau_V(POINT p)
-{
-	POINT p1;
-	
-	p1.x = p.x ; p1.y = LAR;
-	
-	while(p.x < LON)
-	{
-		draw_line(p,p1,noir);
-		p.x = p.x +E;
-		p1.x=p.x;
-	}
-}
-
-void affichage_plateau()
-{
-	POINT p;
-	p.x = 0; p.y = 0;
-	fill_screen(blanc);
-	affichage_plateau_H(p);
-	affichage_plateau_V(p);
-}
-
-int main ()
-{
-	init_graphics(LON,LAR);
-	
-	BOULE b[128];
-	
-	affichage_plateau();
 	
 // Fin du code
 wait_escape();
