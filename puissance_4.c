502638e024ef6cffb9a42f9d48051e3882b5853d
yobero <yoannb94@gmail.com>
2015-04-09 14:12:42 +0200

    Ajout de la grille
    La grille s'adapte partiellement à l'écran

--- puissance_4.c
+++ puissance_4.c
@@ -1,22 +1,45 @@
 #include "graphics.h"
 
-#define l 1200
-#define L 720
+#define LON 1200
+#define LAR 720
+#define R 20
+#define E 50
+#define FOND noir
+#define GRILLE rouge
+#define PB (3*LAR)/4
+#define PH 1-PB
 
 void cercle()
 {
+	POINT p;
+	p.x = E + R; p.y = E + R;
 	
+	while (p.x + E< LON && p.x - E > 0)
+	{
+		while (p.y + E  < PB && p.y -E > 0)
+		{
+			draw_fill_circle(p,R,FOND);
+			p.y = p.y +E;
+		}
+		p.x = p.x +E;
+		p.y = E + R;
+	}
 }
 
 void grille()
 {
+	POINT p1,p2;
+	
+	p1.x = E; p1.y = E;
+	p2.x = LON-E; p2.y = PB -E;
+	draw_fill_rectangle(p1,p2,GRILLE);
 	
 	cercle();
 }
 
 int main ()
 {
-	init_graphics(l,L);
+	init_graphics(LON,LAR);
 	
 	grille();
 	
